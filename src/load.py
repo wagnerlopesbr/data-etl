41c5a42a2d8f53de5a59a283c063f163b24c553b
Wagner Lopes <wagnerlopesbr@hotmail.com>
2025-04-01 11:23:50 -0300

    feat: adjusting the load function

---
+++ src/load.py
@@ -0,0 +1,57 @@
+from typing import Dict, List
+import pandas as pd
+
+
+def dynamic_table_return(table: str, df: pd.DataFrame, columns: List[str]):
+    try:
+        print(f"✅ {table.upper()} return:\n{df[columns].head().to_string(index=False)}\n")
+    except KeyError as e:
+        print(f"☠️ Column not found in {table.upper()}: {e}\n")
+
+
+def page_table(table: str, df: pd.DataFrame):
+    print(f"✅ {table.upper()} return:\n{df[['id', 'name', 'course', 'content_link']].head().to_string(index=False)}\n")
+
+
+def choice_table(table: str, df: pd.DataFrame):
+    matching = df[df["match"]]
+    not_matching = df[~df["match"]]
+    print(f"✅ {table.upper()} rows matching 'Política de Assinatura' or 'Signature Policy':\n{matching[['id', 'name', 'course']].head().to_string(index=False)}\n")
+    print(f"❌ There {'is' if len(not_matching) == 1 else 'are'} {len(not_matching)} row{'s' if len(not_matching) != 1 else ''} not matching.\n")
+
+
+def load(dataframes: Dict[str, pd.DataFrame]):
+    print(f"📰 Starting the loading process...\n\n")
+
+    for table, df in dataframes.items():
+        if df.empty:
+            print(f"⚠️ Warning: DataFrame {table.upper()} is empty.\n")
+            continue
+
+        print(f"✅ DataFrame {table.upper()} extracted successfully with {len(df)} rows.")
+
+        if table == "course":
+            dynamic_table_return(table, df, ['id', 'fullname'])
+        elif table == "course_sections":
+            dynamic_table_return(table, df, ['id', 'name', 'course'])
+        elif table == "course_modules":
+            dynamic_table_return(table, df, ['id', 'course', 'module', 'instance', 'section'])
+        elif table == "modules":
+            dynamic_table_return(table, df, ['id', 'name'])
+        elif table == "feedback":
+            dynamic_table_return(table, df, ['id', 'name', 'course'])
+        elif table == "quiz":
+            dynamic_table_return(table, df, ['id', 'name', 'course'])
+        elif table == "forum":
+            dynamic_table_return(table, df, ['id', 'name', 'course'])
+        elif table == "reengagement":
+            dynamic_table_return(table, df, ['id', 'name', 'course'])
+        elif table == "url":
+            dynamic_table_return(table, df, ['id', 'name', 'course', 'externalurl'])
+        elif table == "page":
+            page_table(table, df)
+        elif table == "choice":
+            choice_table(table, df)
+
+        print(f"✅ DataFrame {table.upper()} has {len(df.columns)} columns: {df.columns.tolist()}.\n")
+    print(f"📰 End of loading process\n\n")
