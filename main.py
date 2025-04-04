87155312ec4848154b7ee5c17a7b7608cc7d50b4
Wagner Lopes <wagnerlopesbr@hotmail.com>
2025-04-04 18:41:31 -0300

    feat: testing to start the process of inserting data into the new db asap

--- main.py
+++ main.py
@@ -6,26 +6,38 @@ from src.transform import transform
 from src.load import load
 from src.logging import start
 from urllib.parse import quote_plus
+from types import SimpleNamespace
+
 
 load_dotenv()
 
 logger = start()
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_PREFIX = os.getenv("DB_PREFIX")
+def get_env_variable(db_nickname):
+    return SimpleNamespace(
+        user=os.getenv(f"{db_nickname}_DB_USER"),
+        password=quote_plus(os.getenv(f"{db_nickname}_DB_PASSWORD")),
+        host=os.getenv(f"{db_nickname}_DB_HOST"),
+        port=os.getenv(f"{db_nickname}_DB_PORT"),
+        name=os.getenv(f"{db_nickname}_DB_NAME"),
+        prefix=os.getenv(f"{db_nickname}_DB_PREFIX")
+    )
 
-engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4")
+old_db = get_env_variable("OLD")
+new_db = get_env_variable("NEW")
+
+
+# Cria os engines usando os dicionários
+old_engine = create_engine(f"mysql+pymysql://{old_db.user}:{old_db.password}@{old_db.host}:{old_db.port}/{old_db.name}?charset=utf8mb4")
+new_engine = create_engine(f"mysql+pymysql://{new_db.user}:{new_db.password}@{new_db.host}:{new_db.port}/{new_db.name}?charset=utf8mb4")
 
 def main():
     try:
+        logger.debug("------------------------------------------ NEW ETL RUN ------------------------------------------")
         logger.debug("ETL process started...")
-        dataframes = extract(engine, DB_PREFIX)
+        dataframes = extract(old_engine, old_db.prefix)
         dataframes = transform(dataframes)
-        load(dataframes)
+        load(dataframes, new_engine, new_db)
         logger.info("ETL process completed successfully!")
     except Exception as e:
         logger.critical(f"ETL process failed: {e}.")
